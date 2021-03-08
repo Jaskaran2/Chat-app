15e614f2dfa8e44aa5386e58da8503cc6dbf84e0
Jaskaran2 <>
2021-03-08 18:40:12 +0530

    adding new files

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+   apiKey: "AIzaSyDKoq5rqU73fJAfHmdfhRH5L9gTjzUXNiM",
+   authDomain: "whatsapp-clone-59fa6.firebaseapp.com",
+   projectId: "whatsapp-clone-59fa6",
+   storageBucket: "whatsapp-clone-59fa6.appspot.com",
+   messagingSenderId: "619109395261",
+   appId: "1:619109395261:web:fcaf32c06c6eb5623f3354",
+   measurementId: "G-3MYDZQXL6J"
+ };
+
+ const  firebaseApp=firebase.initializeApp(firebaseConfig);
+
+ const db=firebaseApp.firestore();
+
+ const auth=firebase.auth();
+
+ const provider =new firebase.auth.GoogleAuthProvider();
+
+ export {auth,provider};
+ export default db;
